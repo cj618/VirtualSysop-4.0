8048e1e7c8c4fa5cd6e8395e59bb614d7bd32378
cj618 <140370920+cj618@users.noreply.github.com>
2024-12-19 17:35:29 +1100

    Development directory structure, game data module

---
+++ prod/data/data.pm
@@ -0,0 +1,106 @@
+package Data;
+
+use strict;
+use warnings;
+
+# Hash to store parsed data
+my %data_store;
+
+# Function to load and parse a file
+sub load_file {
+    my ($filename) = @_;
+
+    # Check if the file exists
+    unless (-e $filename) {
+        die "File not found: $filename\n";
+    }
+
+    # Determine file type based on name and parse accordingly
+    if ($filename =~ /msgsa\.dat$/) {
+        _parse_msg_file($filename, 'msgsa');
+    } elsif ($filename =~ /msgsr\.dat$/) {
+        _parse_msg_file($filename, 'msgsr');
+    } elsif ($filename =~ /msgsv\.dat$/) {
+        _parse_msg_file($filename, 'msgsv');
+    } elsif ($filename =~ /text\.dat$/) {
+        _parse_text_file($filename);
+    } elsif ($filename =~ /virus\.dat$/) {
+        _parse_list_file($filename, 'viruses');
+    } else {
+        warn "Unknown file type: $filename\n";
+    }
+}
+
+# Function to retrieve parsed data
+sub get_data {
+    my ($type) = @_;
+    return $data_store{$type};
+}
+
+# Parse message files (msgsa.dat, msgsr.dat, msgsv.dat)
+sub _parse_msg_file {
+    my ($filename, $key) = @_;
+    open my $fh, '<', $filename or die "Cannot open $filename: $!\n";
+
+    my @entries;
+    my %current_entry;
+
+    while (my $line = <$fh>) {
+        chomp($line);
+        if ($line =~ /^!@#$/) {
+            # Save the current entry if it exists
+            push @entries, {%current_entry} if %current_entry;
+            %current_entry = (action => undef, value => undef, text => '');
+        } elsif ($line =~ /^\d+$/) {
+            if (!defined $current_entry{action}) {
+                $current_entry{action} = $line;
+            } elsif (!defined $current_entry{value}) {
+                $current_entry{value} = $line;
+            }
+        } else {
+            $current_entry{text} .= "$line\n";
+        }
+    }
+
+    # Save the last entry
+    push @entries, {%current_entry} if %current_entry;
+    $data_store{$key} = \@entries;
+
+    close $fh;
+}
+
+# Parse text.dat
+sub _parse_text_file {
+    my ($filename) = @_;
+    open my $fh, '<', $filename or die "Cannot open $filename: $!\n";
+
+    my %text_entries;
+
+    while (my $line = <$fh>) {
+        chomp($line);
+        next if $line =~ /^;/;  # Skip comments
+        if ($line =~ /^(\d{4})\s+(.*)$/) {
+            $text_entries{$1} = $2;
+        }
+    }
+
+    $data_store{text} = \%text_entries;
+    close $fh;
+}
+
+# Parse simple list files like virus.dat
+sub _parse_list_file {
+    my ($filename, $key) = @_;
+    open my $fh, '<', $filename or die "Cannot open $filename: $!\n";
+
+    my @list;
+    while (my $line = <$fh>) {
+        chomp($line);
+        push @list, $line unless $line =~ /^\s*$/;  # Skip empty lines
+    }
+
+    $data_store{$key} = \@list;
+    close $fh;
+}
+
+1; # Return true for module loading
